49091c196732a9e5dfbbaf1f563c0da9d7c8580f
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2025-05-19 16:13:06 -0300

    carrossel video beta

--- web/src/app/page/welcome-page/welcome-page.component.ts
+++ web/src/app/page/welcome-page/welcome-page.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
 import { Router } from '@angular/router';
 import isLogged from '../../services/islogged.service';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { CarrosselFerrariComponent } from '../../components/carrossel-ferrari/carrossel-ferrari.component';
+import { VideoCarrosselComponent } from '../../components/video-carrossel/video-carrossel.component';
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [HeaderComponent, FooterComponent, CarrosselFerrariComponent],
+  imports: [FooterComponent, VideoCarrosselComponent],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
